c43e272e318dd98261e94c2aa92b6f9703d7f7d6
nisaerizkiah <nisaerizkiah@gmail.com>
2025-11-27 16:26:22 +0700

    nisa baru

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,47 @@
-// lib/main.dart
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 import 'services/firebase_services_kifiyah.dart';
 import 'providers/cart_provider_lilis.dart';
-import 'screens/login_screen_ajeng.dart';
-import 'screens/register_screen_ajeng.dart';
-import 'screens/home_screen_ajeng.dart';
-import 'screens/cart_screen_ajeng.dart';
+
+// Screens
+import 'screens/login/login_screen_ajeng.dart';
+import 'screens/register/register_screen_ajeng.dart';
+import 'screens/home/home_screen_ajeng.dart';
+import 'screens/cart/cart_screen_ajeng.dart'; // <-- Tambahkan ini
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  // seed sample products if collection empty
-  await FirebaseServiceKifiyah().seedProducts();
-  runApp(MyApp());
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => CartProviderLilis())],
-      child: MaterialApp(
-        title: 'Smart E-Kantin',
-        theme: ThemeData(
-          primarySwatch: Colors.indigo,
-          fontFamily: 'SatoshiGroupUnique',
-        ),
-        initialRoute: '/',
-        routes: {
-          '/': (context) => LoginScreen_ajeng(),
-          '/register': (context) => RegisterScreen_ajeng(),
-          '/home': (context) => HomeScreen_ajeng(),
-          '/cart': (context) => CartScreen_ajeng(),
-        },
+    return MaterialApp(
+      title: 'Smart E-Kantin',
+      debugShowCheckedModeBanner: false, // Hilangkan label DEBUG
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: "Sora",
+        useMaterial3: true,
       ),
+
+      home: const LoginScreen_ajeng(), // Halaman pertama
+
+      routes: {
+        "/login": (context) => const LoginScreen_ajeng(),
+        "/register": (context) => const RegisterScreen_ajeng(),
+        "/home": (context) => const HomeScreen_ajeng(),
+        "/cart": (context) => const CartScreen_ajeng(), // <-- sekarang cart bisa dipanggil
+      },
     );
   }
 }
