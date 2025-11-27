570bff80bbb7064ae57654a1b938dbea1e72bbb9
kifiyahfaiqotul76 <kifiyahfaiqotul76@gmail.com>
2025-11-27 14:40:50 +0700

    fia new

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,12 @@
+// Top-level Gradle file: android/build.gradle.kts
+
+buildscript {
+    dependencies {
+        // Plugin Google Services (Firebase)
+        classpath("com.google.gms:google-services:4.3.15")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -9,12 +18,14 @@ val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
+
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
